627207b1f14785b0d7398aecc04fa42a5b949300
Alexandre Schritzmeyer <alexschritzmeyer@gmail.com>
2017-06-22 10:13:19 -0300

    Person class annotation review
    Removing some unnecessary fields and class annotation translate

--- Person.cs
+++ Person.cs
@@ -8,106 +8,79 @@ using System.Linq;
 
 namespace Test.Models
 {
-    /// <summary>
-    /// Perfis dos participantes do portal.
-    /// </summary>
     public enum PersonProfile
     {
-        [Display(Name ="Inovador")]
         Innovative,
-
-        [Display(Name = "Mentor")]
         Mentor,
-
-        [Display(Name = "Investidor")]
         Investor,
-
-        [Display(Name = "Empreendedor")]
         Entrepreneur
     }
 
-    /// <summary>
-    /// Classe que representa as pessoas que participam do portal.
-    /// </summary>
     public class Person : ModelBase
     {
-        /// <summary>
-        /// Toda pessoa tem um usuário no sistema.
-        /// </summary>
-        [ForeignKey("User")]
-        [Required(ErrorMessage = "Escolha o usuário associado a essa pessoa (deve ser criado antes)!")]
         public int UserId { get; set; }
         public virtual User User { get; set; }
 
         /// <summary>
-        /// Primeiro nome.
+        /// Person's first name
         /// </summary>
-        [DisplayName("Primeiro nome")]
-        [Required(ErrorMessage = "O nome é obrigatório!")]
+        [DisplayName("First name")]
+        [Required(ErrorMessage = "First name is required!")]
         [StringLength(100)]
         public string FirstName { get; set; }
 
         /// <summary>
-        /// Sobre nome.
+        /// Person's last name
         /// </summary>
-        [DisplayName("Sobrenome")]
-        [Required(ErrorMessage = "O sobrenome é obrigatório!")]
+        [DisplayName("Last name")]
+        [Required(ErrorMessage = "Last name required!")]
         [StringLength(100)]
         public string LastName { get; set; }
 
         /// <summary>
-        /// Cargo atual que ocupa.
+        /// Person's role
         /// </summary>
-        [DisplayName("Cargo")]
-        [Required(ErrorMessage = "O cargo é obrigatório!")]
+        [DisplayName("Role")]
+        [Required(ErrorMessage = "Role is required!")]
         [StringLength(255)]
         public string Role { get; set; }
 
         /// <summary>
-        /// Nome da empresa em que trabalha
+        /// Company name
         /// </summary>
-        [DisplayName("Empresa")]
+        [DisplayName("Company")]
         [StringLength(255)]
         public string Company { get; set; }
 
         /// <summary>
-        /// Número de telefone completo.
+        /// Phone number
         /// </summary>
-        [DisplayName("Telefone")]
-        [Required(ErrorMessage = "O telefone é obrigatório!")]
+        [DisplayName("Phone number")]
+        [Required(ErrorMessage = "Phone number is required")]
         [StringLength(13)]
         public string PhoneNumber { get; set; }
 
         /// <summary>
-        /// Genero.
+        /// Gender
         /// </summary>
-        [DisplayName("Escolha o sexo (opcional)")]
+        [DisplayName("Gender (optional)")]
         public char? Gender { get; set; }
 
         /// <summary>
-        /// Data de nascimento.
+        /// Birth date
         /// </summary>
-        [DisplayName("Data de nascimento")]
-        [Required(ErrorMessage = "A data de nascimento é obrigatória!")]
+        [DisplayName("Date of birth")]
+        [Required(ErrorMessage = "Date of birth is required!")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
         /// <summary>
-        /// Perfil do usuário.
+        /// Profile.
         /// </summary>
-        [DisplayName("Perfil")]
-        [Required(ErrorMessage = "Escolha pelo menos uma das opções de perfil!")]
+        [DisplayName("Profile")]
+        [Required(ErrorMessage = "Profile is required")]
         public PersonProfile Profile { get; set; }
 
-        /// <summary>
-        /// Empresas que a pessoa tem interesse, separadas por ";"
-        /// </summary>
-        [DisplayName("Empresas de interesse")]
-        public string CompaniesOfInterest { get; set; }
-        [StringLength(1000)]
-
-        public virtual ICollection<Activity> Activities { get; set; }
-        public virtual ICollection<Person> Followers { get; set; }
         public virtual ICollection<Message> Messages { get; set; }
         public virtual ICollection<Post> Posts { get; set; }
     }
